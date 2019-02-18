e20db4520a84b45990b6fea27f29f3c449622762
Toai Huynh <pinz.impossible@gmail.com>
2019-02-18 13:02:06 +0700

    fix list of department test

--- server/tests/integration/departmentResolvers/department.test.js
+++ server/tests/integration/departmentResolvers/department.test.js
@@ -1,41 +1,26 @@
 import { expect } from 'chai'
-
-import * as userApi from './userApi'
+import { adminLogin, userLogin } from '../../utils'
 import * as departmentApi from './departmentApi'
 
 describe('departments', () => {
   describe('departments(page: Int, limit: Int!): [Departments]', () => {
-    it('should returns a list of department', async () => {
+    it.only('should returns a list of department', async () => {
       const expectedResult = {
         data: {
           departments: [
             {
-              name: 'NNA',
-              description: 'NNA'
+              description: null,
+              name: 'LKT'
             },
             {
-              name: 'NNA',
-              description: 'NNA'
-            },
-            {
-              name: 'ngôn ngữ anh',
-              description: 'NN'
-            },
-            {
-              name: 'CNTT',
-              description: 'CNTT'
+              name: 'LKT',
+              description: null
             }
           ]
         }
       }
 
-      const {
-        data: {
-          data: {
-            signIn: { token }
-          }
-        }
-      } = await userApi.signIn({ username: 'vinh', password: '123' })
+      const { token } = await adminLogin()
       const result = await departmentApi.departments({ limit: 2 }, token)
 
       expect(result.data).to.eql(expectedResult)
