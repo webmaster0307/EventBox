9de866ce07dfd6aaa468eac3ea6b457a551ee00d
Phat Mai <tienphat1501@gmail.com>
2019-03-05 01:22:57 +0700

    new user-list page

---
+++ client/src/constants/localeConfigs.js
@@ -0,0 +1,152 @@
+export const localeText = {
+  // // for filter panel
+  // page: 'Trang',
+  // more: 'Thêm',
+  // to: 'đến',
+  // of: 'trong',
+  // next: 'Tiếp theo',
+  // last: 'Cuối cùng',
+  // first: 'Đầu tiên',
+  // previous: 'Trước',
+  // loadingOoo: 'Đang tải dữ liệu...',
+
+  // // for set filter
+  // selectAll: 'Chọn tất cả',
+  // searchOoo: 'Tìm kiếm...',
+  // blanks: 'Trống',
+
+  // // for number filter and text filter
+  // filterOoo: 'Lọc...',
+  // applyFilter: 'Áp dụng bộ lọc...',
+  // equals: 'Bằng',
+  // notEquals: 'Khác',
+
+  // // for number filter
+  // lessThan: 'Nhỏ hơn',
+  // greaterThan: 'Lớn hơn',
+  // lessThanOrEqual: 'Nhỏ hơn hoặc bằng',
+  // greaterThanOrEqual: 'Lớn hơn hoặc bằng',
+  // inRange: 'Trong khoảng',
+
+  // // for text filter
+  // contains: 'Bao gồm',
+  // notContains: 'Không bao gồm',
+  // startsWith: 'Bắt đầu bởi',
+  // endsWith: 'Kết thúc bởi',
+
+  // // the header of the default group column
+  // group: 'Nhóm',
+
+  // // tool panel
+  // columns: 'Tùy chỉnh cột',
+  // filters: 'Tủy chỉnh bộ Lọc',
+  // // rowGroupColumns: 'laPivot Cols',
+  // rowGroupColumnsEmptyMessage: 'Kéo thả cột vào đây để nhóm',
+  // // valueColumns: 'laValue Cols',
+  // pivotMode: 'Tùy chỉnh ở chế độ trục',
+  // groups: 'Nhóm cột',
+  // values: 'Tổng hợp',
+  // pivots: 'Trục',
+  // valueColumnsEmptyMessage: 'Kéo thả cột vào đây để tổng hợp',
+  // pivotColumnsEmptyMessage: 'Kéo thả cột vào đây để tủy chỉnh ở chế độ trục',
+  // toolPanelButton: 'la tool panel',
+
+  /* eslint-disable */
+  // other
+  noRowsToShow: '<div>' +
+  '<svg width="105" height="104" viewBox="0 0 116 115" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+    '<path d="M104.058 43.5585C104.139 43.5585 104.219 43.5585 104.3 43.5585C102.771 37.9269 97.6223 33.7434 91.4275 33.7434H30.3647C24.2504 33.7434 19.1015 37.9269 17.4924 43.5585C17.5729 43.5585 17.6533 43.5585 17.7338 43.5585H104.058Z" fill="#4249BE"/>' +
+    '<path d="M64.9591 74.6936L34.4679 77.3485C30.5258 77.6703 27.1468 74.7741 26.7446 70.8319L23.1243 28.5144C22.8025 24.5722 25.6987 21.1933 29.6408 20.791L60.132 18.2166C64.0742 17.8947 67.4531 20.791 67.8554 24.7331L71.4757 66.9702C71.7975 70.9124 68.9012 74.3718 64.9591 74.6936Z" fill="#9EA3F3"/>' +
+    '<path d="M17.7339 49.5126C17.4926 49.5126 17.3317 49.5126 17.0903 49.4321C17.8144 56.1096 23.446 61.339 30.3649 61.339H91.4277C98.266 61.339 103.978 56.1096 104.702 49.4321C104.541 49.5126 104.3 49.5126 104.059 49.5126H17.7339Z" fill="#4249BE"/>' +
+    '<path d="M91.4275 61.3393H30.3647C23.0436 61.3393 17.0098 55.3859 17.0098 47.9843V47.0189C17.0098 39.6978 22.9632 33.6639 30.3647 33.6639H91.4275C98.7486 33.6639 104.782 39.6174 104.782 47.0189V47.9843C104.782 55.3859 98.8291 61.3393 91.4275 61.3393Z" fill="#02BFB1"/>' +
+    '<path d="M100.277 67.6953C100.197 68.6607 99.7945 69.5457 99.0705 70.1893C98.4269 70.8329 97.5419 71.1547 96.6569 71.1547C96.5765 71.1547 96.5765 71.1547 96.496 71.1547L61.7409 69.4652C60.7755 69.3848 59.8905 68.9825 59.2469 68.2584C58.6033 67.5344 58.2815 66.6494 58.2815 65.684L60.695 15.2408C60.7755 13.2295 62.465 11.7009 64.4763 11.8618L99.2314 13.5513C101.243 13.6318 102.771 15.3213 102.61 17.3326L100.277 67.6953Z" fill="#02BFB1"/>' +
+    '<path d="M70.0274 22.402L68.177 22.2411L67.7748 22.9652C67.6943 23.1261 67.6139 23.287 67.6139 23.3675C67.6139 23.4479 67.6139 23.5284 67.6943 23.5284C67.7748 23.6088 67.9357 23.6088 68.177 23.6893V23.8502L66.6484 23.7697V23.6088C66.8898 23.6088 66.9702 23.5284 67.0507 23.5284C67.2116 23.4479 67.2921 23.2066 67.453 22.8848L69.3033 19.0231H69.4642L70.9124 23.1261C70.9928 23.4479 71.1537 23.6893 71.2342 23.7697C71.3146 23.8502 71.4755 23.9306 71.7169 23.9306V24.0915L69.7861 24.0111V23.8502C69.947 23.8502 70.1079 23.8502 70.1883 23.7697C70.2688 23.6893 70.2688 23.6893 70.2688 23.6088C70.2688 23.5284 70.2688 23.3675 70.1883 23.1261L70.0274 22.402ZM69.947 22.0802L69.2229 20.0689L68.2575 21.9998L69.947 22.0802Z" fill="#E3E4F7"/>' +
+    '<path d="M96.0939 25.5393C96.0134 25.5393 96.0134 25.5393 96.0939 25.5393L75.096 24.4935C74.9351 24.4935 74.7742 24.3326 74.7742 24.1717C74.7742 24.0108 74.9351 23.8499 75.096 23.8499L96.0939 24.8957C96.2548 24.8957 96.4157 25.0566 96.4157 25.2175C96.3352 25.4589 96.2548 25.5393 96.0939 25.5393Z" fill="#E3E4F7"/>' +
+    '<path d="M95.8523 30.6079C95.7719 30.6079 95.7719 30.6079 95.8523 30.6079L66.407 29.1597C66.2461 29.1597 66.0852 28.9988 66.0852 28.8379C66.0852 28.677 66.2461 28.5161 66.407 28.5161L95.8523 29.9642C96.0132 29.9642 96.1741 30.1251 96.1741 30.286C96.0937 30.447 96.0132 30.6079 95.8523 30.6079Z" fill="#E3E4F7"/>' +
+    '<path d="M95.5305 35.5956L66.0852 34.1475C65.9243 34.1475 65.7634 33.9866 65.7634 33.8257C65.7634 33.6648 65.9243 33.5039 66.0852 33.5039L95.5305 34.952C95.6914 34.952 95.8523 35.1129 95.8523 35.2738C95.8523 35.4347 95.7719 35.5956 95.5305 35.5956Z" fill="#E3E4F7"/>' +
+    '<path d="M95.2893 40.5841L65.844 39.1359C65.6831 39.1359 65.5222 38.975 65.5222 38.8141C65.5222 38.6532 65.6831 38.4923 65.844 38.4923L95.2893 39.9404C95.4502 39.9404 95.6111 40.1013 95.6111 40.2622C95.6111 40.5036 95.4502 40.5841 95.2893 40.5841Z" fill="#E3E4F7"/>' +
+    '<path d="M95.0479 45.5717L65.6026 44.1236C65.4417 44.1236 65.2808 43.9627 65.2808 43.8018C65.2808 43.6409 65.4417 43.48 65.6026 43.48L95.0479 44.9281C95.2088 44.9281 95.3697 45.089 95.3697 45.2499C95.3697 45.4913 95.2088 45.5717 95.0479 45.5717Z" fill="#DFE1F7"/>' +
+    '<path d="M94.8064 50.6403L65.3611 49.1922C65.2002 49.1922 65.0393 49.0313 65.0393 48.8704C65.0393 48.7095 65.2002 48.5486 65.3611 48.5486L94.8064 49.9967C94.9673 49.9967 95.1282 50.1576 95.1282 50.3185C95.1282 50.4794 94.9673 50.6403 94.8064 50.6403Z" fill="#4DB6AC"/>' +
+    '<path d="M23.0436 27.3094L24.9744 49.6749L69.6251 45.8133L67.5333 22.4018C67.292 19.586 64.798 17.4943 61.9822 17.7356L28.5143 20.7123C25.2158 21.1146 22.7218 24.0109 23.0436 27.3094Z" fill="#99D3F9"/>' +
+    '<path d="M36.8009 48.7095L45.5701 38.2507L42.9956 36.0785C40.9039 34.3086 37.7663 34.55 35.9964 36.7222L25.0549 49.7553L36.8009 48.7095Z" fill="#C8E3F5"/>' +
+    '<path d="M61.9019 29.5626C59.0057 27.1491 54.6613 27.5513 52.1673 30.4476L45.5703 38.3319L36.801 48.7101L55.4658 47.1011L69.7057 47.0206L68.6599 35.2747L61.9019 29.5626Z" fill="#60AFE4"/>' +
+    '<path d="M45.5701 31.5731C47.4363 31.5731 48.9491 30.0603 48.9491 28.1942C48.9491 26.328 47.4363 24.8152 45.5701 24.8152C43.704 24.8152 42.1912 26.328 42.1912 28.1942C42.1912 30.0603 43.704 31.5731 45.5701 31.5731Z" fill="#C8E3F5"/>' +
+    '<path opacity="0.1" d="M104.782 47.0992C104.782 45.8924 104.622 44.6856 104.3 43.5593C104.219 43.5593 104.139 43.5593 104.058 43.5593H17.7338C17.6534 43.5593 17.5729 43.5593 17.4925 43.5593C17.1707 44.6856 17.0098 45.8924 17.0098 47.0992V48.0646C17.0098 48.5473 17.0098 49.03 17.0902 49.4323C17.2511 49.5127 17.4925 49.5127 17.7338 49.5127H104.139C104.38 49.5127 104.541 49.5127 104.782 49.4323C104.863 48.9496 104.863 48.4669 104.863 48.0646V47.0992H104.782Z" fill="#212121"/>' +
+    '<path opacity="0.1" d="M101.243 97.3017H20.5496C20.0669 97.3017 19.5842 96.8994 19.5842 96.3363V96.2558C19.5842 95.7731 19.9865 95.2904 20.5496 95.2904H101.323C101.806 95.2904 102.289 95.6927 102.289 96.2558V96.3363C102.208 96.819 101.806 97.3017 101.243 97.3017Z" fill="#263238"/>' +
+    '<path d="M99.4728 99.0707H22.3197C19.9061 99.0707 17.8144 97.3007 17.6535 94.9676L14.1136 51.3629C14.0332 50.1561 14.4354 49.0298 15.2399 48.1448C16.1249 47.1794 17.4121 46.6162 18.7798 46.6162H103.013C104.38 46.6162 105.668 47.1794 106.553 48.1448C107.357 49.0298 107.759 50.1561 107.679 51.3629L104.139 94.9676C103.978 97.3007 101.886 99.0707 99.4728 99.0707Z" fill="url(#paint0_linear)"/>' +
+    '<path opacity="0.1" d="M100.358 95.6914H21.4347C19.9061 95.6914 18.458 94.9674 17.573 93.9215L17.6534 94.9674C17.8144 97.22 19.9061 99.0704 22.3196 99.0704H99.3923C101.806 99.0704 103.898 97.3005 104.059 94.9674L104.139 93.9215C103.334 94.9674 101.967 95.6914 100.358 95.6914Z" fill="#212121"/>' +
+    '<path d="M49.4319 1V5.02258" stroke="#95F0C1" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>' +
+    '<path d="M51.4432 3.01172H47.4207" stroke="#95F0C1" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>' +
+    '<path opacity="0.2" d="M113.874 65.6025C114.496 65.6025 115 65.0982 115 64.4762C115 63.8541 114.496 63.3499 113.874 63.3499C113.252 63.3499 112.747 63.8541 112.747 64.4762C112.747 65.0982 113.252 65.6025 113.874 65.6025Z" stroke="#4B4668" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>' +
+    '<path opacity="0.2" d="M66.7289 109.931L64.2349 114.115H69.1424L66.7289 109.931Z" stroke="#4B4668" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>' +
+    '<path opacity="0.2" d="M2.44813 46.455L1 49.0294H3.97671L2.44813 46.455Z" stroke="#4B4668" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>' +
+    '<defs>' +
+      '<linearGradient id="paint0_linear" x1="105.587" y1="106.794" x2="-18.7106" y2="13.8726" gradientUnits="userSpaceOnUse">' +
+        '<stop stop-color="#00A296"/>' +
+        '<stop offset="1" stop-color="#6EF8EE"/>' +
+      '</linearGradient>' +
+    '</defs>' +
+  '</svg>' +
+  '<p>Không có dữ liệu</p></div>'
+  /* eslint-enable */
+
+  // // enterprise menu
+  // pinColumn: 'Ghim cột',
+  // // valueAggregation: 'laValue Agg',
+  // autosizeThiscolumn: 'Tự động căn chỉnh cột này',
+  // autosizeAllColumns: 'Tự động căn chỉnh tất cả cột',
+  // groupBy: 'Nhóm bởi',
+  // ungroupBy: 'Không nhóm bởi',
+  // resetColumns: 'Làm mới tất cả cột',
+  // expandAll: 'Xem tất cả',
+  // collapseAll: 'Xổ xuống tất cả',
+  // toolPanel: 'Thanh công cụ',
+  // export: 'Xuất file',
+  // csvExport: 'Xuất CSV',
+  // excelExport: 'Xuất Excel',
+
+  // // enterprise menu pinning
+  // pinLeft: 'Ghim trái',
+  // pinRight: 'Ghim phải',
+  // noPin: 'Bỏ ghim',
+
+  // // enterprise menu aggregation and status bar
+  // sum: 'Tổng',
+  // min: 'Nhỏ nhất',
+  // max: 'Lớn nhất',
+  // none: 'Không có giá trị',
+  // count: 'Số lượng',
+  // average: 'Trung bình',
+
+  // // standard menu
+  // copy: 'Sao chép',
+  // copyWithHeaders: 'Sao chép bao gồm tên cột',
+  // ctrlC: 'Ctrl + C',
+  // paste: 'Dán',
+  // ctrlV: 'Ctrl + V'
+}
+
+/* eslint-disable */
+export const icons = {
+  groupExpanded: '<span style="width: 25px;height: 25px; text-align: center;margin-left: 8px;"><i style="font-weight: bolder;" class="fa fa-angle-down"></i></span>',
+  groupContracted: '<span style="width: 25px;height: 25px; text-align: center;margin-left: 8px;"><i style="font-weight: bolder;" class="fa fa-angle-right"></i></span>'
+}
+/* eslint-enable */
+
+export const postProcessPopup = (params) => {
+  let ePopup = params.ePopup
+  let oldTopStr = ePopup.style.top
+  let oldLeftStr = ePopup.style.left
+  oldTopStr = oldTopStr.substring(0, oldTopStr.indexOf('px'))
+  oldLeftStr = oldLeftStr.substring(0, oldLeftStr.indexOf('px'))
+  let oldTop = parseInt(oldTopStr)
+  let oldLeft = parseInt(oldLeftStr)
+  let newTop = oldTop + 16
+  let newLeft = oldLeft + 9
+  ePopup.style.top = newTop + 'px'
+  ePopup.style.left = newLeft + 'px'
+}
+
+export const getRowHeight = (params) => {
+  return params.node.level === 1 ? 300 : 40
+}
